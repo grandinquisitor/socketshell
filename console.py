4db792debdb555c5476fd1c2747a6e41b8479a93
nick <infinityminusnine@gmail.com>
2009-11-20 20:26:25 -0500

    added documentation

--- console.py
+++ console.py
@@ -1,11 +1,33 @@
+"""
+ShellServer (or SocketShell?)
+
+Live, multi-user threaded remote python shell access.
+
+Interact with live python code as it is running, or fight with other users and try to crash each other's threads.
+
+Usage:
+
+>>> s = ShellServer('localhost', 6666)
+>>> print s.server_address
+>>> s.serve_forever()
+
+
+$ telnet localhost 6666
+Python 2.5.1 (r251:54863, Jun 17 2009, 20:37:34) 
+[GCC 4.0.1 (Apple Inc. build 5465)] on darwin
+Type "help", "copyright", "credits" or "license" for more information.
+(MyConsole)
+>>>
+
+"""
+
+
 import code
 import SocketServer
 import threading
 import sys
 
-foo = 1
-
-class MyConsole(code.InteractiveConsole):
+class _MyConsole(code.InteractiveConsole):
     def __init__(self, rfile, wfile, filename="<console>"):
         code.InteractiveConsole.__init__(self, None, filename)
         self.rfile = rfile
@@ -51,24 +73,46 @@ class MyConsole(code.InteractiveConsole):
             if self._softspace(self.wfile, 0):
                 print >>self.wfile
 
-class MyTCPRequestHandler(SocketServer.StreamRequestHandler):
+class _MyTCPRequestHandler(SocketServer.StreamRequestHandler):
 
     def handle(self):
         self.wfile.flush()
-        console = MyConsole(self.rfile, self.wfile)
+        console = _MyConsole(self.rfile, self.wfile)
         console.interact()
 
 
 class ShellServer(SocketServer.ThreadingMixIn, SocketServer.TCPServer):
+    """
+    ShellServer class
+
+    Usage Example:
+
+    >>> s = ShellServer('localhost', 0)
+    >>> print s.server_address
+    >>> s.serve_forever()
+    """
+
     def __init__(self, host, port):
-        SocketServer.TCPServer.__init__(self, (host, port), MyTCPRequestHandler)
+        SocketServer.TCPServer.__init__(self, (host, port), _MyTCPRequestHandler)
 
 
 class AsyncShellServer(threading.Thread, ShellServer):
+    """
+    A ShellServer class that will run in the background without
+    blocking anything
+
+    Usage Example:
+
+    >>> s = AsyncShellServer('localhost', 0)
+    >>> print s.server_address
+    >>> s.setDaemon(True)
+    >>> s.start()
+    >>> 
+    """
+
     def __init__(self, host, port):
         ShellServer.__init__(self, host, port)
         threading.Thread.__init__(self, target=self.serve_forever)
-        self.setDaemon(True)
 
 
 
