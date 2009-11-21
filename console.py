68eba57eb58db751a708ae1df28b2b465b05104e
nick <infinityminusnine@gmail.com>
2009-11-21 11:58:24 -0500

    banner, docs, clui
    + changed banner
    + more docs
    + command-line args
    + changed default startup to ShellServer rather than AsyncShellServer
    + added start() as an alias for serve_forever
    + added daemon flag to AsyncShellServer constructor

--- console.py
+++ console.py
@@ -1,7 +1,9 @@
+#!/usr/bin/env python
+
 """
 ShellServer (or SocketShell?)
 
-Live, multi-user threaded remote python shell access.
+Live, multi-user threaded remote python shell.
 
 Interact with live python code as it is running, or fight with other users and try to crash each other's threads.
 
@@ -19,8 +21,10 @@ Type "help", "copyright", "credits" or "license" for more information.
 (MyConsole)
 >>>
 
+AsyncShellServer runs in its own thread. ShellServer runs in the current thread.
 """
 
+__version__ = 'DEV'
 
 import code
 import SocketServer
@@ -43,6 +47,7 @@ class _MyConsole(code.InteractiveConsole):
         input = self.rfile.next().rstrip("\n\r")
         return input
 
+    # not sure if I even need this
     @staticmethod
     def _softspace(file, newvalue):
         oldvalue = 0
@@ -60,6 +65,7 @@ class _MyConsole(code.InteractiveConsole):
 
     def runcode(self, code):
         try:
+            # uh, sys is threadsafe, right?
             sys.stdout = self.wfile
             sys.stderr = self.wfile
             exec code in globals()
@@ -78,7 +84,14 @@ class _MyTCPRequestHandler(SocketServer.StreamRequestHandler):
     def handle(self):
         self.wfile.flush()
         console = _MyConsole(self.rfile, self.wfile)
-        console.interact()
+
+        banner = "%s\n<<%s %s on %s serving %s>>" % \
+            (sys.version, 
+            self.server.__class__.__name__, hex(id(self.server)),
+            ':'.join(map(str, self.connection.getsockname())),
+            ':'.join(map(str, self.client_address)))
+
+        console.interact(banner=banner)
 
 
 class ShellServer(SocketServer.ThreadingMixIn, SocketServer.TCPServer):
@@ -92,14 +105,16 @@ class ShellServer(SocketServer.ThreadingMixIn, SocketServer.TCPServer):
     >>> s.serve_forever()
     """
 
-    def __init__(self, host, port):
+    def __init__(self, port, host='127.0.0.1'):
         SocketServer.TCPServer.__init__(self, (host, port), _MyTCPRequestHandler)
+
+    def start(self):
+        self.serve_forever()
 
 
 class AsyncShellServer(threading.Thread, ShellServer):
     """
-    A ShellServer class that will run in the background without
-    blocking anything
+    A ShellServer class that will run in the background without blocking the current thread
 
     Usage Example:
 
@@ -108,19 +123,65 @@ class AsyncShellServer(threading.Thread, ShellServer):
     >>> s.setDaemon(True)
     >>> s.start()
     >>> 
+
+    Set daemon=True on the constructor or call setDaemon(True) on the object to have the server thread exit when the main thread exits if inactive. See the documentation on threading.Thread, of which this class is a subclass.
     """
 
-    def __init__(self, host, port):
-        ShellServer.__init__(self, host, port)
+    def __init__(self, port, host='127.0.0.1', daemon=False):
+        ShellServer.__init__(self, port, host)
         threading.Thread.__init__(self, target=self.serve_forever)
+        if daemon:
+            self.setDaemon(True)
 
 
 
 if __name__ == "__main__":
-    # Port 0 means to select an arbitrary unused port
-    HOST, PORT = "0.0.0.0", 6669
+    from optparse import OptionParser
+    parser = OptionParser(
+        usage="%prog [options] port",
+        version="%%prog %s" % __version__,
+        add_help_option=False)
 
-    server = ShellServer(HOST, PORT)
-    print "running on", server.server_address
+    parser.add_option('--help', action='help', help='show this message')
 
-    server.serve_forever()
+    parser.add_option('-q', 
+                    dest='quiet',
+                    help='suppress startup messages',
+                    action='store_true')
+
+    parser.add_option('-h', '--host',
+                    dest="hostname",
+                    default='127.0.0.1',
+                    help="hostname to bind to. use 0.0.0.0 for any.\n[default: %default]",
+                    metavar="HOST")
+
+    parser.add_option("-p", '--port', 
+                    dest="port",
+                    help="port to use. use 0 for self-assigned port [default].",
+                    type='int',
+                    default=0,
+                    metavar="PORT")
+
+    (flags, args) = parser.parse_args()
+
+    if args:
+        if len(args) > 1:
+            parser.error("uncrecognized options: %r" % args)
+        elif flags.port and args:
+            parser.error("conflicting port settings: %r and %r" % (flags.port, args))
+        else:
+            try:
+                port = int(args[0])
+            except ValueError:
+                parser.error("invalid integer value: %r" % (args[0],))
+
+    else:
+        port = flags.port
+
+    host = flags.hostname
+
+    server = ShellServer(port, host)
+    if not flags.quiet:
+        print "running on", ':'.join(map(str, server.server_address))
+
+    server.start()
