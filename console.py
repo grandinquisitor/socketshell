92a76d5b577f1765e223e40dc1ba4de2e0dd5966
nick <infinityminusnine@gmail.com>
2009-11-21 16:56:05 -0500

    logging that you can turn on and off

--- console.py
+++ console.py
@@ -79,19 +79,93 @@ class _MyConsole(code.InteractiveConsole):
             if self._softspace(self.wfile, 0):
                 print >>self.wfile
 
+
+class _Tee(object):
+    """
+    wrapper object that takes two file-like objects and tees all calls to write() and next() from one to the other
+    """
+
+    def __init__(self, inputfile, outputfile, format=None):
+        self.inputfile = inputfile
+        self.outputfile = outputfile
+        self.format = format or (lambda x: x)
+        self.linebuf = ''
+
+    def __getattribute__(self, attrname):
+        try:
+            return object.__getattribute__(self, attrname)
+        except AttributeError:
+            return getattr(object.__getattribute__(self, 'inputfile'), attrname)
+
+    def write(self, data):
+        self.outputfile.write(self.format(data))
+        return self.inputfile.write(data)
+
+    def next(self):
+        line = self.inputfile.next()
+        self.outputfile.write(self.format(line))
+        return line
+
 class _MyTCPRequestHandler(SocketServer.StreamRequestHandler):
 
+    def __init__(self, request, client_address, server, logging=False):
+        self.logging = logging
+        SocketServer.StreamRequestHandler.__init__(self, request, client_address, server)
+
+    @classmethod
+    def factoryfactory(cls, logging=True):
+        """
+        farcical thing that lets us add arguments to the constructor even though we don't have control of how/where it is called
+        """
+        def factory(request, client_address, server):
+            instance = cls(request, client_address, server, logging)
+            return instance
+
+        return factory
+
+
+    @staticmethod
+    def _get_formatter(addr, dir):
+        def formatter(line):
+           return '-- ' + client_address_str + ' ' + dir + ' ' + line.rstrip() + "\n"
+
+
     def handle(self):
-        self.wfile.flush()
-        console = _MyConsole(self.rfile, self.wfile)
+
+        client_address_str = ':'.join(map(str, self.client_address))
+        server_address_str = ':'.join(map(str, self.connection.getsockname()))
+
+        if self.logging:
+            print >>sys.stderr, "-- client", client_address_str, "connected"
+            inputoutput = (
+                _Tee(self.rfile, sys.stderr, self._get_formatter(client_address_str, '<')),
+                _Tee(self.wfile, sys.stderr, self._get_formatter(client_address_str, '>'))
+                )
+
+        else:
+            inputoutput = (self.rfile, self.wfile)
+
+        # self.wfile.flush()
+            
+        console = _MyConsole(*inputoutput)
 
         banner = "%s\n<<%s %s on %s serving %s>>" % \
             (sys.version, 
             self.server.__class__.__name__, hex(id(self.server)),
-            ':'.join(map(str, self.connection.getsockname())),
-            ':'.join(map(str, self.client_address)))
+            server_address_str,
+            client_address_str
+            )
+        
 
-        console.interact(banner=banner)
+        try:
+            console.interact(banner=banner)
+
+        except StopIteration:
+            if self.logging:
+                print >>sys.stderr, "-- client", client_address_str, "exited"
+        except SocketServer.socket.error, e:
+            if self.logging:
+                print >>sys.stderr, "-- client", client_address_str, "disconnected"
 
 
 class ShellServer(SocketServer.ThreadingMixIn, SocketServer.TCPServer):
@@ -105,8 +179,8 @@ class ShellServer(SocketServer.ThreadingMixIn, SocketServer.TCPServer):
     >>> s.serve_forever()
     """
 
-    def __init__(self, port, host='127.0.0.1'):
-        SocketServer.TCPServer.__init__(self, (host, port), _MyTCPRequestHandler)
+    def __init__(self, port, host='127.0.0.1', logging=True):
+        SocketServer.TCPServer.__init__(self, (host, port), _MyTCPRequestHandler.factoryfactory(logging=logging))
 
     def start(self):
         self.serve_forever()
@@ -127,7 +201,7 @@ class AsyncShellServer(threading.Thread, ShellServer):
     Set daemon=True on the constructor or call setDaemon(True) on the object to have the server thread exit when the main thread exits if inactive. See the documentation on threading.Thread, of which this class is a subclass.
     """
 
-    def __init__(self, port, host='127.0.0.1', daemon=False):
+    def __init__(self, port, host='127.0.0.1', logging=True, daemon=False):
         ShellServer.__init__(self, port, host)
         threading.Thread.__init__(self, target=self.serve_forever)
         if daemon:
@@ -166,7 +240,7 @@ if __name__ == "__main__":
 
     if args:
         if len(args) > 1:
-            parser.error("uncrecognized options: %r" % args)
+            parser.error("unrecognized options: %r" % args)
         elif flags.port and args:
             parser.error("conflicting port settings: %r and %r" % (flags.port, args))
         else:
@@ -180,8 +254,9 @@ if __name__ == "__main__":
 
     host = flags.hostname
 
-    server = ShellServer(port, host)
+    server = ShellServer(port, host, logging=not flags.quiet)
+
     if not flags.quiet:
-        print "running on", ':'.join(map(str, server.server_address))
+        print >>sys.stderr, "running on", ':'.join(map(str, server.server_address))
 
     server.start()
