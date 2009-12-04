546c32cc84953107d63d6225a46d3ca23c51378e
nick <infinityminusnine@gmail.com>
2009-12-04 11:51:10 -0500

    moving stuff

--- setup.py
+++ setup.py
@@ -2,6 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='socketshell',
+    url='http://github.com/grandinquisitor/socketshell',
     version='0.1',
     packages=find_packages(),
     )
